72a277b3d57c839184df0ef2f87afcdbcab4e20b
Felipe <dev.felipesnunes@gmail.com>
2025-08-28 18:33:41 -0300

    fix: alter sqlite to postgresql

--- prisma/migrations/20250828205752_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "emails" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "email" TEXT NOT NULL,
-    "isIOS" BOOLEAN NOT NULL DEFAULT false,
-    "adType" TEXT,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL
-);
